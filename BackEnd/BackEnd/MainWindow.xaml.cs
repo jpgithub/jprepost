6a9aac3a1157076f1c04a9eabcac63831d00c70f
jpgithub <jqpan00@gmail.com>
2014-10-24 15:03:36 -0700

    Check In

--- BackEnd/BackEnd/MainWindow.xaml.cs
+++ BackEnd/BackEnd/MainWindow.xaml.cs
@@ -22,86 +22,146 @@ namespace BackEnd
     /// </summary>
     public partial class MainWindow : Window
     {
-        private uint count = 0;
+        private int count = 0;
         private DispatcherTimer frametimer = new DispatcherTimer();
         private double frameInterval = 32;
         private DispatcherTimer timer;
         private List<FrameObject> Frames = new List<FrameObject>();
-            
+        private bool isReserve;
+        private bool isSingleStep;
+        private bool isPause;
+        public delegate void QueryTime();
+    
         public MainWindow()
         {
             InitializeComponent();
             timer = new DispatcherTimer();
             timer.Interval = TimeSpan.FromSeconds(1);
             timer.Tick += timer_Tick;
-            //timer.Tick += frame_Tick;
-            //timer.Start();
+            timer.Start();
 
             frametimer.Interval = TimeSpan.FromMilliseconds(32);
             frametimer.Tick += frame_Tick;
+
             //frametimer.Start();
-            FrameObject test = new FrameObject();
-
-            byte[] teststream = { 0xff,0x00,0xff,0xff,0xff,0x00,0x00 };
-
-            FrameParser.SvaFrameParser((uint)FrameObject.Versions.V14, teststream, out test);
-
-            new TctFileReader(string.Empty);
-
-            Frames.Add(test);
+            //FrameObject test = new FrameObject();
+            //byte[] teststream = { 0xff,0x00,0xff,0xff,0xff,0x00,0x00 };
+            //FrameParser.SvaFrameParser((uint)FrameObject.Versions.V14, teststream, out test);
+            //new TctFileReader(string.Empty);
+            //Frames.Add(test);
 
         }
 
         private void frame_Tick(object sender, EventArgs e)
         {
             FrameTime.Content = "Frame " + count.ToString();
-            count++;
-            if (count > 25)
-                FrameSet.Content = count.ToString();
+            
+            if (isReserve)
+                count--; // if Frame.First() == begin call stop
+            else
+                count++;
+            
+            if ((count > 25) || (count < -25))
+                count = 0;
         }
 
         private void timer_Tick(object sender, EventArgs e)
         {
             lblTime.Content = DateTime.Now.ToLongTimeString();
-            count = 1;
         }
 
         private void rewindBtn_Click(object sender, RoutedEventArgs e)
         {
-            // Wrong Implementation
-            frameInterval *= 2;
-            frametimer.Interval = TimeSpan.FromMilliseconds(frameInterval); 
+            if (!isSingleStep)
+            {
+                isReserve = true;
+                StartDispatcher();
+            }
+
+            if (isSingleStep)
+            {
+                count--;
+                FrameTime.Content = "Frame " + count.ToString();
+            }
         }
-        public delegate void QueryTime();
+        
         private void playBtn_Click(object sender, RoutedEventArgs e)
         {
-            playBtn.Dispatcher.BeginInvoke(DispatcherPriority.Normal, new QueryTime(CheckTime));
-            //if (!frametimer.IsEnabled)
-            //    frametimer.Start();
+            frameInterval = 32;
+            isReserve = false;
+            StartDispatcher();  
+        }
 
-            //frameInterval = 32;
-            //frametimer.Interval = TimeSpan.FromMilliseconds(frameInterval);
-   
+        private void StartDispatcher()
+        {
+            if (!frametimer.IsEnabled)
+            {
+                frametimer.Start();
+            }
+            frametimer.Interval = TimeSpan.FromMilliseconds(frameInterval);
         }
 
         private void CheckTime()
         {
             if (!timer.IsEnabled)
+            {
                 timer.Start();
-            
+            }
         }
 
         private void forwardBtn_Click(object sender, RoutedEventArgs e)
         {
-            frameInterval /= 2;
-            frametimer.Interval=TimeSpan.FromMilliseconds(frameInterval); 
+            if (!isSingleStep)
+            {
+                isReserve = false;
+                frameInterval /= 2;
+                StartDispatcher();
+            }
+
+            if (isSingleStep)
+            {
+                count++;
+                FrameTime.Content = "Frame " + count.ToString();
+            }
         }
 
         private void pauseBtn_Click(object sender, RoutedEventArgs e)
         {
-            frametimer.Stop();
+            if (!isPause)
+            {
+                frametimer.Stop();
+                isPause = true;
+            }
+            else
+            {
+                isPause = false;
+                StartDispatcher();
+            }
         }
 
+        private void stopBtn_Click(object sender, RoutedEventArgs e)
+        {
+            frametimer.Stop();
+            isReserve = false;
+            //DisplayOutput(Frame.First()); 
+            count = 0;
+            FrameTime.Content = "Frame " + count.ToString();
+        }
 
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            isSingleStep = true;
+            playBtn.IsEnabled = false;
+            stopBtn.IsEnabled = false;
+            pauseBtn.IsEnabled = false;
+        }
+
+        private void CheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            isSingleStep = false;
+            playBtn.IsEnabled = true;
+            stopBtn.IsEnabled = true;
+            pauseBtn.IsEnabled = true;
+        }
     }
 }
