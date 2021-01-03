490163f23bb4ad9dbada7ca9ce124c66798e309c
jpgithub <jqpan00@gmail.com>
2021-01-03 12:02:01 -0800

    Check in new exercise
    Task Delay and Continue

---
+++ CATaskDelayExcerise/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace CATaskDelayExercise
+{
+    class Program
+    {
+        static void Print()
+        {
+            Console.WriteLine(string.Format("Hello World!,{0} \n Current Time: {1} ", Task.CurrentId, DateTime.UtcNow));
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(string.Format("Hello World!,{0} \n Current Time: {1} ", Task.CurrentId, DateTime.UtcNow));
+            Task.Factory.ContinueWhenAll(new Task[1] { Task.Delay(5000) } , completedTasks =>
+            {
+                Print();
+            }).Wait();
+        }
+    }
+}
