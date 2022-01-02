82ee13df7b49823986fdd362a5f0d15bed7580f2
jpgithub <jqpan00@gmail.com>
2022-01-01 16:14:05 -0800

    Create InsertionSort.cs
    check in

---
+++ RootedTreeEx/Sorting/InsertionSort.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Sorting
+{
+    public static class InsertionSort
+    {
+        public static void Sort(List<int> sequence)
+        {
+			
+            //// Ascending order
+            for(int i = 0; i < sequence.Count; i++)
+            {
+                var temp = sequence[i];
+                int selectedindex = -1;
+                for (int j = i - 1; j >= 0; j--)
+                {
+                    if(sequence[j] > temp)
+                    {
+                        selectedindex = j;
+                        //sequence.RemoveAt(i);
+                        //sequence.Insert(0,temp);
+                    }
+                }
+
+                if(selectedindex >= 0)
+                {
+                    sequence.RemoveAt(i);
+                    sequence.Insert(selectedindex,temp);
+                }
+
+            }
+        }
+	}
+}
