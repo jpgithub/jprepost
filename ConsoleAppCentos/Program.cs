78da449055f83c1359441f5401e74b9f7cf1577c
jpgithub <jqpan00@gmail.com>
2022-04-17 20:31:57 -0700

    Update Program.cs

--- ConsoleAppCentos/Program.cs
+++ ConsoleAppCentos/Program.cs
@@ -2,6 +2,10 @@
 
 namespace ConsoleAppCentos
 {
+    /*
+     dotnet publish -c release -r rhel-x64 --self-contained
+     dotnet publish -c debug -r rhel-x64 --self-contained
+     */
     class Program
     {
         static void Main(string[] args)
